0f2fb2a2a1ea8735364aad0b3449f103adcab5ac
David <davidhuangdw@163.com>
2014-12-23 22:51:36 +0800

    forget adjust serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :password_digest, :auth_token, :bio, :bio_update_count
+  attributes :id, :name, :bio, :bio_update_count
 end
